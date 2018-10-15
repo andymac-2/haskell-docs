5180184f8e7552e68149e8fe0e5210dcf1bb20ae
Kahlil Abreu <kahlil29@gmail.com>
2018-10-15 13:25:16 +0530

    Removed redundant  and replaced  with guards

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -2,29 +2,21 @@ module FizzBuzz where
 
 
 fizzBuzz35 :: [String]
-fizzBuzz35 = do
-  fmap replaceNumbers [1..100]
+fizzBuzz35 = fmap replaceNumbers [1..100]
  where
-  replaceNumbers currentNumber = 
-    if (rem currentNumber 15 == 0)
-    then "FizzBuzz"
-    else if (rem currentNumber 3 == 0)
-         then "Fizz"
-         else if (rem currentNumber 5 == 0)
-              then "Buzz"
-              else show currentNumber   
+  replaceNumbers currentNumber
+    | (rem currentNumber 15 == 0) = "FizzBuzz"
+    | (rem currentNumber 3 == 0)  = "Fizz"
+    | (rem currentNumber 5 == 0)  = "Buzz"
+    | otherwise                   = show currentNumber   
 
 
 fizzBuzzGeneric :: Int -> Int ->  [String]
-fizzBuzzGeneric fizz buzz = do
-  fmap replaceNumbers [1..100]
+fizzBuzzGeneric fizz buzz = fmap replaceNumbers [1..100]
  where
-  replaceNumbers currentNumber = 
-    if (rem currentNumber (fizz * buzz) == 0)
-    then "FizzBuzz"
-    else if (rem currentNumber fizz == 0)
-         then "Fizz"
-         else if (rem currentNumber buzz == 0)
-              then "Buzz"
-              else show currentNumber   
+  replaceNumbers currentNumber 
+    | (rem currentNumber (fizz * buzz) == 0) = "FizzBuzz"
+    | (rem currentNumber fizz == 0)          = "Fizz"
+    | (rem currentNumber buzz == 0)          = "Buzz"
+    | otherwise                              = show currentNumber   
 
